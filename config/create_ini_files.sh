3242b54581f9a68385d148f92a8958762cb6fcb4
kshannon <k.shannon.ucsd@gmail.com>
2018-06-23 12:00:13 -0700

    Add sample data, with same file structure and update .sh for [sample]

--- config/create_ini_files.sh
+++ config/create_ini_files.sh
@@ -1,4 +1,11 @@
 cat <<EOT >> "data_path.ini"
+[sample]
+train_image_paths = ../sample_data/train_image_paths.csv
+train_labeled_studies = ../sample_data/train_labeled_studies.csv
+valid_image_paths = ../sample_data/valid_image_paths.csv
+valid_labeled_studies = ../sample_data/valid_labeled_studies.csv
+train_imgs = ../sample_data/train/
+valid_imgs = ../sample_data/valid/
 [csv]
 train_image_paths = <enter/path>
 train_labeled_studies = <enter/path>
