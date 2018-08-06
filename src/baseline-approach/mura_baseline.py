07ba7001e1eb88dc5c942d26ee5afa0ed57011ad
kshannon <k.shannon.ucsd@gmail.com>
2018-08-05 21:37:38 -0700

    Add model epoch and step parameters from paper

--- src/baseline-approach/mura_baseline.py
+++ src/baseline-approach/mura_baseline.py
@@ -16,28 +16,22 @@ then averaged to output the probability of abnormality for the study.'''
 '''final fully connected layer has a single output, after which we applied a sigmoid nonlinearity.'''
 #[3]
 '''Before feeding images into the network, we normalized each image to have the same mean and
-standard deviation of images in the ImageNet training set. We then scaled the variable-sized images
-to 320 × 320. We augmented the data during training by applying random lateral inversions and
-rotations of up to 30 degrees.'''
+standard deviation of images in the ImageNet training set.'''
 #[4]
-'''The weights of the network were initialized with weights from a model pretrained on ImageNet
-(Deng et al., 2009). The network was trained end-to-end using Adam with default parameters β1 =
-0.9 and β2 = 0.999 (Kingma & Ba, 2014).'''
+'''We augmented the data during training by applying random lateral inversions and
+rotations of up to 30 degrees.'''
 #[5]
-'''We trained the model using minibatches of size 8. We
-used an initial learning rate of 0.0001 that is decayed by a factor of 10 each time the validation loss
+'''Learning rate decayed by a factor of 10 each time the validation loss
 plateaus after an epoch. We ensembled the 5 models with the lowest validation losses.'''
 
 #### TODOs.......
-#TODO [1]
+#TODO [1] -- FOR INFERENCE SCRIPT
 # baseline model makes predictions based on arithematic mean of a view...
 # might need to turn split_data_labels() into a dict to capture the view info?
 # or only for inference step.... think about this.
 #TODO [2]
 # normalize to ImageNet mean/std ?? found this online?? [ 103.939, 116.779, 123.68 ]
 # normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],std=[0.229, 0.224, 0.225])
-#TODO [3]
-# add model checkpointing ability, arg parse and fileself.
 #TODO [4]
 # add print to logging: https://docs.python.org/3.7/library/logging.html
 
@@ -87,6 +81,7 @@ except:
 #### ========= Global Vars and Constants ========= ####
 MAX_DATA = args.max_data
 MODEL_SUMMARY = args.model_summary
+EPOCHS = 30
 IMG_RESIZE_X = 320
 IMG_RESIZE_Y = 320
 BATCH_SIZE = 8
@@ -164,10 +159,17 @@ def img_augmentation(image, label):
 
 def build_dataset(data, labels):
     """ TODO """
+    print('===============fhjdkslfhjvl=========')
+
     # labels = tf.cast(labels, tf.uint8) #do I really need to do this.....
-    labels = tf.one_hot(tf.cast(labels, tf.uint8), 2) #do I really need to do this.....
+    labels = tf.one_hot(tf.cast(labels, tf.uint8), 2) #cast labels to dim 2 tf obj
+    # labels = tf.cast(labels, tf.uint8) #cast labels to dim 2 tf obj DEBUG
+    # print(labels.shape)
+    # labels = tf.reshape(labels, [labels.shape,-1])
+    # print(labels.shape)
+    # sys.exit() DEBUG
     dataset = tf.data.Dataset.from_tensor_slices((data, labels))
-    dataset = dataset.shuffle(len(data))
+    dataset = dataset.shuffle(len(data)) #mioght not need this.... DEBUG
     dataset = dataset.map(preprocess_img, num_parallel_calls=4) #TODO num_parallel_calls?
     # dataset = dataset.map(img_augmentation, num_parallel_calls=4)
     dataset = dataset.batch(BATCH_SIZE) # (?, x, y) unknown batch size because the last batch will have fewer elements.
@@ -184,21 +186,6 @@ def build_dataset(data, labels):
 def main():
     train_dataset = build_dataset(train_imgs, train_labels)
     valid_dataset = build_dataset(valid_imgs, valid_labels)
-    #DEBUG start
-    print(type(train_dataset))
-    iterator = train_dataset.make_one_shot_iterator()
-    # for x, y in iterator:
-    #     print(type(x))
-    #     print(x.shape)
-    #     print(y.shape)
-    #     print(y)
-    #     # z = x.numpy()
-    #     # print(z.max())
-    #     # print(z.min())
-    #     print('==========================$$$$$$$$========================')
-    #     break
-    # sys.exit() DEBUG
-
 
     print("Downloading DenseNet PreTrained Weights...")
     # https://keras.io/applications/#densenet
@@ -209,7 +196,7 @@ def main():
             pooling='max',
             classes=2)
     last_layer = DenseNet169.output
-    print(last_layer)
+    # print(last_layer)
     preds = tf.keras.layers.Dense(2, activation='sigmoid')(last_layer)
     model = tf.keras.Model(DenseNet169.input, preds)
 
@@ -242,26 +229,57 @@ def main():
 
     print("Beginning to Train Model")
     model.fit(train_dataset,
-            epochs=3,
-            steps_per_epoch=3, #steps_per_epoch=patches_len // batch_size...
+            epochs=EPOCHS,
+            steps_per_epoch=(len(train_labels)//BATCH_SIZE),
             validation_data=valid_dataset, #.make_one_shot_iterator()...?
-            validation_steps=3,
-            callbacks=[checkpointer, tensorboard]) #tb_log --> RuntimeError: Merging tf.summary.* ops is not compatible with eager execution. Use tf.contrib.summary instead
+            validation_steps=16,
+            shuffle=True,
+            callbacks=[checkpointer]) #tb_log --> RuntimeError: Merging tf.summary.* ops is not compatible with eager execution. Use tf.contrib.summary instead
     sys.exit()
 
 
 if __name__ == '__main__':
     main()
-
-# # Generate seperate lists of img paths and labels to feed into tf.data
-
 #
-# # Build tf.data objects to interact with tf.iterator
-# train_dataset = build_dataset(train_imgs, train_labels) #training data
-# valid_dataset = build_dataset(valid_imgs, valid_labels) #validation data
-# print(train_dataset)
-
+#  roc curve plotting:
+# from sklearn.metrics import roc_curve
+# y_pred_keras = keras_model.predict(valid_dataset).ravel()
+# fpr_keras, tpr_keras, thresholds_keras = roc_curve(y_test, y_pred_keras)
 #
-# loss = tf.losses.sigmoid_cross_entropy(
-#         multi_class_labels=tf.squeeze(tf.one_hot(labels, depth=2), axis=1),
-#         logits=logits)
+# # from sklearn.metrics import auc
+# # auc_keras = auc(fpr_keras, tpr_keras)
+#
+#
+#
+# from sklearn.ensemble import RandomForestClassifier
+# # Supervised transformation based on random forests
+# rf = RandomForestClassifier(max_depth=3, n_estimators=10)
+# rf.fit(X_train, y_train)
+#
+# y_pred_rf = rf.predict_proba(X_test)[:, 1]
+# fpr_rf, tpr_rf, thresholds_rf = roc_curve(y_test, y_pred_rf)
+# auc_rf = auc(fpr_rf, tpr_rf)
+#
+#
+#
+# plt.figure(1)
+# plt.plot([0, 1], [0, 1], 'k--')
+# plt.plot(fpr_keras, tpr_keras, label='Keras (area = {:.3f})'.format(auc_keras))
+# plt.plot(fpr_rf, tpr_rf, label='RF (area = {:.3f})'.format(auc_rf))
+# plt.xlabel('False positive rate')
+# plt.ylabel('True positive rate')
+# plt.title('ROC curve')
+# plt.legend(loc='best')
+# plt.show()
+# # Zoom in view of the upper left corner.
+# plt.figure(2)
+# plt.xlim(0, 0.2)
+# plt.ylim(0.8, 1)
+# plt.plot([0, 1], [0, 1], 'k--')
+# plt.plot(fpr_keras, tpr_keras, label='Keras (area = {:.3f})'.format(auc_keras))
+# plt.plot(fpr_rf, tpr_rf, label='RF (area = {:.3f})'.format(auc_rf))
+# plt.xlabel('False positive rate')
+# plt.ylabel('True positive rate')
+# plt.title('ROC curve (zoomed in at top left)')
+# plt.legend(loc='best')
+# plt.show()
