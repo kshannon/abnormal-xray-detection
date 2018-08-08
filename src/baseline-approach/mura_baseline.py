c0360d093cb2840e95dbbd0b207d60887df2c41e
kshannon <k.shannon.ucsd@gmail.com>
2018-08-08 00:26:35 -0700

    Add new keras class Adam optimizer with simple decay

--- src/baseline-approach/mura_baseline.py
+++ src/baseline-approach/mura_baseline.py
@@ -35,6 +35,7 @@ plateaus after an epoch. We ensembled the 5 models with the lowest validation lo
 import sys
 import os
 import time
+from random import randint
 import argparse
 from configparser import ConfigParser
 import h5py
@@ -74,14 +75,14 @@ except:
 #### ========= Global Vars and Constants ========= ####
 MAX_DATA = args.max_data
 MODEL_SUMMARY = args.model_summary
-EPOCHS = 30
+EPOCHS = 15
 IMG_RESIZE_X = 320
 IMG_RESIZE_Y = 320
 CHANNELS = 3
 BATCH_SIZE = 8
 PREFETCH_SIZE = 1
 LEARNING_RATE = 0.0001
-DECAY_FACTOR = 10 #learnng rate decayed when valid. loss plateaus after an epoch
+# DECAY_FACTOR = 10 #learnng rate decayed when valid. loss plateaus after an epoch
 ADAM_B1 = 0.9 #adam optimizer default beta_1 value (Kingma & Ba, 2014)
 ADAM_B2 = 0.999 #adam optimizer default beta_2 value (Kingma & Ba, 2014)
 MAX_ROTAT_DEGREES = 30 #up to 30 degrees img rotation.
@@ -138,8 +139,9 @@ def preprocess_img(filename, label):
 def img_augmentation(image, label):
     """ Call this on minibatch at time of training """
     image = tf.image.random_flip_left_right(image) #lateral inversion with P(0.5)
-    # rotatse up to 30 https://www.tensorflow.org/api_docs/python/tf/contrib/image/rotate
-    #TODO
+    image = tf.image.rot90(image, k=randint(0, 4)) #not 0-30 degrees, but 90 degree increments... so sue me!
+    #TODO # rotatse up to 30 https://www.tensorflow.org/api_docs/python/tf/contrib/image/rotate
+    # https://www.tensorflow.org/api_docs/python/tf/keras/preprocessing/image/random_rotation
     image = tf.clip_by_value(image, 0.0, 1.0) #ensure [0.0,1.0] img constraint
     return image, label
 
@@ -148,23 +150,20 @@ def build_dataset(data, labels):
     labels = tf.one_hot(tf.cast(labels, tf.uint8), 1) #cast labels to dim 2 tf obj
     dataset = tf.data.Dataset.from_tensor_slices((data, labels))
     dataset = dataset.shuffle(len(data)) #mioght not need this.... DEBUG
-    dataset = dataset.repeat(EPOCHS)
+    dataset = dataset.repeat()
     dataset = dataset.map(preprocess_img, num_parallel_calls=2) 
     dataset = dataset.map(img_augmentation, num_parallel_calls=2)
     dataset = dataset.batch(BATCH_SIZE) # (?, x, y) unknown batch size because the last batch will have fewer elements.
-    # dataset = dataset.prefetch(PREFETCH_SIZE) #single training step consumes n elements
+    dataset = dataset.prefetch(PREFETCH_SIZE) #single training step consumes n elements
     return dataset
 
 
-
-#rotation.. https://www.tensorflow.org/api_docs/python/tf/keras/preprocessing/image/random_rotation
-
-
+#### ========= Training & Fitting ========= ####
 def main():
-    # with tf.device('/cpu:0'):
-    print("Building Train/Validation Dataset Objects")
-    train_dataset = build_dataset(train_imgs, train_labels)
-    valid_dataset = build_dataset(valid_imgs, valid_labels)
+    with tf.device('/cpu:0'):
+        print("Building Train/Validation Dataset Objects")
+        train_dataset = build_dataset(train_imgs, train_labels)
+        valid_dataset = build_dataset(valid_imgs, valid_labels)
 
     print("Downloading DenseNet PreTrained Weights... Might take ~0:30 seconds")
     DenseNet169 = tf.keras.applications.densenet.DenseNet169(include_top=False,
@@ -182,6 +181,11 @@ def main():
     optimizer = tf.train.AdamOptimizer(learning_rate=LEARNING_RATE,
             beta1=ADAM_B1,
             beta2=ADAM_B2)
+    
+    optimizer_keras = tf.keras.optimizers.Adam(lr=LEARNING_RATE,
+            beta_1=ADAM_B1,
+            beta_2=ADAM_B2,
+            decay=0.10)
 
     # https://www.tensorflow.org/api_docs/python/tf/keras/callbacks/ModelCheckpoint
     checkpointer = tf.keras.callbacks.ModelCheckpoint(filepath=CHECKPOINT_FILENAME,
@@ -191,14 +195,14 @@ def main():
 
     # https://www.tensorflow.org/api_docs/python/tf/keras/callbacks/TensorBoard
     tensorboard = tf.keras.callbacks.TensorBoard(log_dir=TB_LOG_DIR,
-            histogram_freq=1,
-            batch_size=BATCH_SIZE,
+            # histogram_freq=1, #this screwed us over... caused tensorboard callback to fail.. why??? DEBUG !!!!!!
+            # batch_size=BATCH_SIZE, # and take this out... and boom.. histogam frequency works. sob
             write_graph=True,
-            write_grads=True,
+            write_grads=False,
             write_images=True)
 
     print("Compiling Model!")
-    model.compile(optimizer=optimizer,
+    model.compile(optimizer=optimizer_keras,
             loss='binary_crossentropy',
             metrics=['accuracy'])
 
@@ -209,11 +213,11 @@ def main():
     print("Beginning to Train Model")
     model.fit(train_dataset,
             epochs=EPOCHS,
-            steps_per_epoch=(len(train_labels)//BATCH_SIZE),
-            verbose=2,
+            steps_per_epoch=(len(train_labels)//BATCH_SIZE), #36808 train number
+            verbose=1,
             validation_data=valid_dataset,
-            validation_steps=4,#(len(valid_labels)//BATCH_SIZE),
-            callbacks=[checkpointer,tensorboard]) 
+            validation_steps= (len(valid_labels)//BATCH_SIZE),  #3197 validation number
+            callbacks=[checkpointer,tensorboard])  #https://towardsdatascience.com/learning-rate-schedules-and-adaptive-learning-rate-methods-for-deep-learning-2c8f433990d1
             
         
     # Save entire model to a HDF5 file
@@ -226,6 +230,10 @@ def main():
 if __name__ == '__main__':
     main()
 #
+
+# *Note:
+# WARNING:tensorflow:TensorFlow optimizers do not make it possible to access optimizer attributes or optimizer state after instantiation. As a result, we cannot save the optimizer as part of the model save file.You will have to compile your model again after loading it. Prefer using a Keras optimizer instead (see keras.io/optimizers).
+
 #  roc curve plotting:
 # from sklearn.metrics import roc_curve
 # y_pred_keras = keras_model.predict(valid_dataset).ravel()
